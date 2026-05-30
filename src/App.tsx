1d2c85d075ded1bce65436a9552da98fdff024ff
Sebastian <sebas@gmail.com>
2026-05-29 23:18:53 -0500

    feat: Estructura inicial del Landing Page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import HomeLogin from './pages/HomeLogin'
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+      <HomeLogin />
+    </>
+  )
+}
+
+export default App
